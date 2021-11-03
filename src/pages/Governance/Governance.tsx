0ee87240f38a6c35a89f8d9b2bfd86255676a034
Octavio Amuchástegui <octavioamuchastegui@gmail.com>
2021-11-03 20:39:55 -0300

    test action

--- src/pages/Governance/Governance.tsx
+++ src/pages/Governance/Governance.tsx
@@ -254,7 +254,7 @@ function Governance() {
 
         <div>
           <h1 className="font-serif text-brandblue text-center text-5xl my-5">
-            DELEGATE VOTES
+            DELEGATE VOTES TEST
           </h1>
           <p className="text-2xl text-center">
             For multisig wallets, see instructions{" "}
