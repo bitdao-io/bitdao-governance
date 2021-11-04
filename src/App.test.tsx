ff643412cf16932584a2ef1210503ce726e851cb
octavioamu <octavioamuchastegui@gmail.com>
2021-11-04 00:13:32 -0300

    fix app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders DELEGATE VOTES title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/DELEGATE VOTES/i);
   expect(linkElement).toBeInTheDocument();
 });
