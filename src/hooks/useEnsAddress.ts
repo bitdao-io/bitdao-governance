2bc62f3594e83fa353e7cf02ae524751298def4d
octavioamu <octavioamuchastegui@gmail.com>
2021-11-03 23:55:14 -0300

    fix lint issues

--- src/hooks/useEnsAddress.ts
+++ src/hooks/useEnsAddress.ts
@@ -1,6 +1,3 @@
-import { utils } from 'ethers';
-import { useState, useEffect } from 'react';
-
 const UseEnsAddress = async (provider: any, ens: string) => {
   try {
     const ensAddress = await provider.resolveName(ens);
